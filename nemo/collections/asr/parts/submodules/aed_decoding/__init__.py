3dfd9440953c92ecf4440e823255f87e36f9cc9d
andrusenkoau <andrusenkoau@users.noreply.github.com>
2025-09-22 12:48:49 +0000

    Apply isort and black reformatting
    Signed-off-by: andrusenkoau <andrusenkoau@users.noreply.github.com>

--- nemo/collections/asr/parts/submodules/aed_decoding/__init__.py
+++ nemo/collections/asr/parts/submodules/aed_decoding/__init__.py
@@ -14,8 +14,8 @@
 
 from nemo.collections.asr.parts.submodules.aed_decoding.aed_batched_streaming import (
     GreedyBatchedStreamingAEDComputer,
-    return_decoder_input_ids,
     initialize_aed_model_state,
+    return_decoder_input_ids,
 )
 
 __all__ = [
