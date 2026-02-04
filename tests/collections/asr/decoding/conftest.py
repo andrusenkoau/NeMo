66a26fb8fa75095d1ed338e9c603e6092a3d2ad8
Vladimir Bataev <vbataev@nvidia.com>
2026-02-04 13:38:10 +0400

    Move dataset fixtures

--- tests/collections/asr/decoding/conftest.py
+++ tests/collections/asr/decoding/conftest.py
@@ -16,45 +16,9 @@ from pathlib import Path
 import pytest
 
 from nemo.collections.asr.models import ASRModel
-from nemo.collections.asr.parts.utils.manifest_utils import read_manifest, write_manifest
 from tests.collections.asr.decoding.utils import make_preprocessor_deterministic, preserve_decoding_cfg_and_cpu_device
 
 CHECKPOINTS_PATH = Path("/home/TestData/asr")
-
-
-@pytest.fixture(scope="session")
-def an4_val_manifest_corrected(tmp_path_factory, test_data_dir):
-    """
-    Correct an4_val manifest audio filepaths, e.g.,
-    "tests/data/asr/test/an4/wav/an440-mjgm-b.wav" -> test_data_dir / "test/an4/wav/an440-mjgm-b.wav"
-    """
-    an4_val_manifest_orig_path = Path(test_data_dir) / "asr/an4_val.json"
-    an4_val_manifest_corrected_path = tmp_path_factory.mktemp("manifests") / "an4_val_corrected.json"
-    an4_val_records = read_manifest(an4_val_manifest_orig_path)
-    for record in an4_val_records:
-        record["audio_filepath"] = record["audio_filepath"].replace(
-            "tests/data/asr", str(an4_val_manifest_orig_path.resolve().parent)
-        )
-    write_manifest(an4_val_manifest_corrected_path, an4_val_records)
-    return an4_val_manifest_corrected_path
-
-
-@pytest.fixture(scope="session")
-def an4_train_manifest_corrected(tmp_path_factory, test_data_dir):
-    """
-    Correct an4_train manifest audio filepaths, e.g.,
-    "tests/data/asr/test/an4/wav/an440-mjgm-b.wav" -> test_data_dir / "test/an4/wav/an440-mjgm-b.wav"
-    """
-    an4_train_manifest_orig_path = Path(test_data_dir) / "asr/an4_train.json"
-    an4_train_manifest_corrected_path = tmp_path_factory.mktemp("manifests") / "an4_train_corrected.json"
-    an4_train_records = read_manifest(an4_train_manifest_orig_path)
-    for record in an4_train_records:
-        record["audio_filepath"] = record["audio_filepath"].replace(
-            "tests/data/asr", str(an4_train_manifest_orig_path.resolve().parent)
-        )
-    write_manifest(an4_train_manifest_corrected_path, an4_train_records)
-    return an4_train_manifest_corrected_path
-
 
 @pytest.fixture(scope="package")
 def _stt_en_conformer_transducer_small_raw():
